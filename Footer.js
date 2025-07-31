8c89853732310dedf705fe6d8ef3a90cd8f5df9d
AkaliKhoTinh <150569237+AkaliKhoTinh@users.noreply.github.com>
2025-07-31 14:36:50 +0700

    Footer.js

---
+++ Footer.js
@@ -0,0 +1,12 @@
+//Footer
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={{ padding: '1rem', background: '#eee', marginTop: '2rem', textAlign: 'center' }}>
+      <p>© 2025 Nhóm Lập trình React - Trường NTTU</p>
+    </footer>
+  );
+};
+
+export default Footer;
